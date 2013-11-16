f340c0c3c06bfcadb8e633d75393ad2137924619
photino <panzan89@gmail.com>
2013-11-16 21:37:01 +0800

    Design Media Page

--- js/wm-dom.js
+++ js/wm-dom.js
@@ -9,14 +9,16 @@
 
   $(document).ready(function() {
 
+    $('input, textarea').placeholder();
+
     $('.wm-sns-share img').on('click', function() {
-      var img = $(this).attr('src').replace(/(.+\/)/g, ''),
-        sns = {'renren': 'http://share.renren.com/share/buttonshare.do?link=',
-          'douban': 'http://www.douban.com/recommend/?url=',
-          'sina': 'http://v.t.sina.com.cn/share/share.php?url=',
-          'qzone': 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=',
-          'tencent': 'http://share.v.t.qq.com/index.php?c=share&a=index&url='};
-      window.open(sns[img.slice(0,-4)] + encodeURIComponent(document.location.href) +
+      var sns = $(this).attr('alt'),
+        links = {'人人': 'http://share.renren.com/share/buttonshare.do?link=',
+          '豆瓣': 'http://www.douban.com/recommend/?url=',
+          '新浪微博': 'http://v.t.sina.com.cn/share/share.php?url=',
+          'QQ空间': 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=',
+          '腾讯微博': 'http://share.v.t.qq.com/index.php?c=share&a=index&url='};
+      window.open(links[sns] + encodeURIComponent(document.location.href) +
         '&amp;title=' + encodeURIComponent(document.title));
     });
 
@@ -34,10 +36,6 @@
 
   $(window).load(function() {
 
-    if($('body').height() < $(window).height()) {
-      $('footer').addClass('navbar-fixed-bottom');
-    }
-
     $(window).scroll(function () {
       if($(this).scrollTop() > $(window).height()) {
         if(!$('.wm-back-to-top').length) {
@@ -50,16 +48,19 @@
     });
 
     $('.wm-gallery img').css('height',
-      $('.wm-gallery img').eq(0).css('height')
+      $('.wm-gallery img').eq(0).height() || 240
     );
 
     $('.wm-recommend img').css('height',
-      $('.wm-recommend img').eq(0).css('height')
+      $('.wm-recommend img').eq(0).height() || 120
     );
 
     $('.wm-show-video a').on('click', function() {
       var video = $(this).attr('href'),
         width = $('main article').width();
+      if($('.wm-show-video').length > 1) {
+        $('.wm-video .wm-video-title').text($(this).text());
+      }
       if(!$('.wm-video iframe').length) {
         $('.wm-video').append('<iframe src="http://player.youku.com/"></iframe>');
       }
